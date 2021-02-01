9e890545d22b9e2bc78431f75dc70a022c0ac532
Th3You7 <elbasiriy@gmail.com>
2021-02-01 01:11:38 +0100

    adding server workers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
 import MainProvider from "./Providers/MainProvider";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./hooks/scrollToTop";
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./index.css";
 
@@ -20,3 +21,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
